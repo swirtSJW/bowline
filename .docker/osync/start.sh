a220ddd6e07df3ddbb954d24a00f7d05588ae4ee
David Numan <david.numan@civicactions.com>
2016-09-21 14:36:45 -0400

    Corrections to sync and to file ownership

--- .docker/osync/start.sh
+++ .docker/osync/start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Verify we have enough max_user_watches
+if [ $(cat /proc/sys/fs/inotify/max_user_watches) -lt 100000 ]; then
+  echo -e "\n\nYou must increase your max user watches allowed for osync to monitor all the files.\n"
+  echo -e "On linux systems, you can do this:\n  sudo sysctl fs.inotify.max_user_watches=100100\n"
+  exit 1
+fi
 
-# First do initial sync to filestore
+# First wait for other containers to start, then do initial sync to filestore
+sleep 3
+rm -rfv /var/www/.osync_workdir/state
 osync.sh /etc/osync/sync.conf
 
 # Start daemon mode to monitor changes
--- .docker/sshd/docker-entrypoint.sh
+++ .docker/sshd/docker-entrypoint.sh
@@ -8,4 +8,14 @@ if [ "$PASSWORD" ]; then
   echo "${USER}:${PASSWORD}" | chpasswd
 fi
 
+if [ "$HOST_USER" ]; then
+  echo Setting the ${USER} user to ${HOST_USER}
+  OWNER=$(echo $HOST_USER | cut -d: -f1)
+  GROUP=$(echo $HOST_USER | cut -d: -f2)
+  if [ "$OWNER" != "0" ]; then
+    usermod -o -u $OWNER ${USER}
+    groupmod -o -g $GROUP ${USER}
+  fi
+fi
+
 exec "$@"
